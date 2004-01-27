73291a70c2b07cad2b33d942ff2a242c5c5d172c
Nicolas S. Dade <nic.dade@gmail.com>
2004-01-27 02:16:07 +0000

    added Id line

--- pwsafe.cpp
+++ pwsafe.cpp
@@ -3,6 +3,8 @@
 
    Copyright (C) 2004 Nicolas S. Dade
 
+   $Id$
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2, or (at your option)
