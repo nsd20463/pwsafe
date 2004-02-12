15117bb4c10c37c23d47708c1d54ce8505e25306
Nicolas S. Dade <nic.dade@gmail.com>
2004-02-12 15:21:46 +0000

    system.h moved back b/c it was making a mess with its #defines

--- pwsafe.cpp
+++ pwsafe.cpp
@@ -21,8 +21,9 @@
 
 */
 
-#include <sys/types.h>
-#include "system.h"
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -38,11 +39,13 @@
 #include <signal.h>
 #endif
 #include <getopt.h>
+#include <sys/types.h>
 #include <errno.h>
 #include <pwd.h>
 #include <regex.h>
 #include <sys/mman.h>
 #include <limits.h>
+
 
 #include <string>
 #include <map>
@@ -50,6 +53,7 @@
 #include <algorithm>
 #include <memory>
 
+#include "system.h"
 
 #include <termios.h>
 
@@ -84,6 +88,7 @@ static char* readline(const char*);
 #include <X11/Xmu/Atoms.h>
 #include <X11/Xmu/WinUtil.h>
 #endif
+
 
 // The name the program was run with, stripped of any leading path
 const char *program_name = "pwsafe"; // make sure program_name always points to something valid so we can use it in constructors of globals
